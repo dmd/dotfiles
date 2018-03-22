e19ddb567018e751a2ce3e70fe4a4fef0942c905
Daniel M. Drucker <ddrucker@abinitio.com>
2018-03-22 14:07:36 -0400

    emacs

---
+++ base/.emacs.d/init.el
@@ -0,0 +1,16 @@
+(menu-bar-mode 0)
+(setq auto-mode-alist (append '(("\\.run$" . xxtrace-mode)) auto-mode-alist))
+(load-library "/u/saletnik/public/xxtrace.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(xterm-mouse-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
