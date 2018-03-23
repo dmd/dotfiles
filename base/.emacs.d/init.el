a6bdd35bf591a0602777865366b72c6cf6cb25a1
Daniel M. Drucker <ddrucker@abinitio.com>
2018-03-23 13:08:25 -0400

    y-or-n

--- base/.emacs.d/init.el
+++ base/.emacs.d/init.el
@@ -1,6 +1,9 @@
+
 (menu-bar-mode 0)
 (setq auto-mode-alist (append '(("\\.run$" . xxtrace-mode)) auto-mode-alist))
 (load-library "/u/saletnik/public/xxtrace.el")
+(defalias 'yes-or-no-p 'y-or-n-p)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
