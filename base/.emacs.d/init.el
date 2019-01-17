2f866fe52aaa66bb4573841337ea404120ac97f3
Daniel Drucker <ddrucker@micc.cm.cluster>
2019-01-17 15:42:23 -0500

    no more ab init

--- base/.emacs.d/init.el
+++ base/.emacs.d/init.el
@@ -1,12 +1,5 @@
 (setq auto-save-list-file-prefix nil)
 (menu-bar-mode 0)
-(setq auto-mode-alist (append '(("\\.run$" . xxtrace-mode)) auto-mode-alist))
-
-(let ((xxtrace-highlight "/u/saletnik/public/xxtrace.el"))
- (when (file-exists-p xxtrace-highlight)
-   (load-file xxtrace-highlight))
-)
-
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
